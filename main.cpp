c2c9235d4a02cc86ae16ab7369a76d5af357ece2
ujjwalVishwakarma2006 <2023ucs0116@iitjammu.ac.in>
2025-12-09 21:19:06 +0530

    Increase Abstraction with hittable.h, rtweekend.h, sphere.h and interval.h

--- main.cpp
+++ main.cpp
@@ -1,21 +1,14 @@
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "rtweekend.h"
 
-#include <iostream>
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = center - r.origin(); // C - P
-    auto a = dot(r.direction(), r.direction()); // d . d
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius * radius; 
-    auto discriminant = b*b - 4*a*c; 
-    return (discriminant >= 0);
-}
-
-color ray_color(const ray& r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) 
-        return color(1, 0, 0);
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, interval(0, infinity), rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
+    }
     
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
@@ -32,6 +25,12 @@ int main() {
     // Calculate the image height, and ensure that it's at least 1.
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    // World
+
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
 
@@ -66,7 +65,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
